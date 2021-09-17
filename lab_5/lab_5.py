7027cbf198d90d1e8f3d97df72e3ee4daf951643
aleksei <alex.zedfeelgood@gmail.com>
2021-09-17 14:50:47 +0300

    added 5 lab

--- lab_5/lab_5.py
+++ lab_5/lab_5.py
@@ -1,21 +1,16 @@
-import numpy as np
 import scipy.stats as stats
 from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
+import src_lab_5 as lab_src
+import src
 
 samples_sizes = [20, 60, 100]
 N = 1000
 
-covs = [
-    [[1, 0],
-     [0, 1]],
+path = "paper/src/"
+rhos = [0, 0.5, 0.9]
 
-    [[1, 0.5],
-     [0.5, 1]],
-
-    [[1, 0.9],
-     [0.9, 1]],
-]
+covs = [[[1, rho], [rho, 1]] for rho in rhos]
 
 distributions = [multivariate_normal([0, 0], cov) for cov in covs]
 
@@ -26,5 +21,62 @@ mixed_covs = [
     [[10, -0.9],
      [-0.9, 10]],
 ]
+mixed_distribution = lab_src.MixtureModel([multivariate_normal([0, 0], mixed_cov) for mixed_cov in mixed_covs],
+                                          [0.9, 0.1])
 
-mixed_distribution = 0.9 * stats.multivariate_normal.rvs([0, 0], mixed_covs[0]) + 0.1 * stats.multivariate_normal.rvs([0, 0], mixed_covs[1])
+# for i in range(len(samples_sizes)):
+#     r, r_q, r_s = lab_src.get_coefficients(distributions=distributions, sample_size=samples_sizes[i], N=N)
+#     rows_sub_names = [r"$E(z)$", r"$E(z^2)$", r"$D(z)$", ""]
+#     rows_names = [r"$\rho = " + str(rhos[j // 5]) + "$" if j % 5 == 0 else rows_sub_names[j % 5 - 1] for j in
+#                   range(1, 14)]
+#     columns_names = ['$r$', r"$r_s$", r"$r_Q$"]
+#     r_result, r_s_result, r_Q_result = lab_src.help_find_mean(r, rhos), lab_src.help_find_mean(r_s,
+#                                                                                                rhos), lab_src.help_find_mean(
+#         r_q, rhos)
+#     value_matrix = []
+#     for j in range(len(rhos)):
+#         sub_matrix = []
+#         sub_matrix.append(r_result[j])
+#         sub_matrix.append(r_s_result[j])
+#         sub_matrix.append(r_Q_result[j])
+#         sub_matrix = [[sub_matrix[j][i] for j in range(len(sub_matrix))] for i in range(len(sub_matrix[0]))]
+#         value_matrix = value_matrix + sub_matrix
+#
+#     value_matrix.insert(3, [""] * len(columns_names))
+#     value_matrix.insert(4, columns_names)
+#     value_matrix.insert(8, [""] * len(columns_names))
+#     value_matrix.insert(9, columns_names)
+#     src.create_latex_table(table_name=path + "CovRhoSampleSize" + str(samples_sizes[i]),
+#                            angle_elem=r"$\rho=$" + str(rhos[0]), rows_names=rows_names, columns_names=columns_names,
+#                            values_matrix=value_matrix, tolerance=4)
+# value_matrix = []
+# for i in range(len(samples_sizes)):
+#     r, r_q, r_s = lab_src.get_coefficients(distributions=[mixed_distribution], sample_size=samples_sizes[i], N=N)
+#     r_result, r_s_result, r_Q_result = lab_src.help_find_mean(r, [0]), lab_src.help_find_mean(r_s, [
+#         0]), lab_src.help_find_mean(r_q, [0])
+#     sub_matrix = [r_result[0], r_s_result[0], r_Q_result[0]]
+#     sub_matrix = [[sub_matrix[j][i] for j in range(len(sub_matrix))] for i in range(len(sub_matrix[0]))]
+#     value_matrix = value_matrix + sub_matrix
+#
+# rows_sub_names = [r"$E(z)$", r"$E(z^2)$", r"$D(z)$", ""]
+# rows_names = [r"$n = " + str(samples_sizes[j // 5]) + "$" if j % 5 == 0 else rows_sub_names[j % 5 - 1] for j in
+#               range(1, 14)]
+# columns_names = ['$r$', r"$r_s$", r"$r_Q$"]
+#
+# value_matrix.insert(3, [""] * len(columns_names))
+# value_matrix.insert(4, columns_names)
+# value_matrix.insert(8, [""] * len(columns_names))
+# value_matrix.insert(9, columns_names)
+#
+# src.create_latex_table(table_name=path + "CovMixedSamples",
+#                        angle_elem=r"$n=$" + str(samples_sizes[0]), rows_names=rows_names, columns_names=columns_names,
+#                        values_matrix=value_matrix, tolerance=4)
+
+titles = ["ρ = 0", "ρ = 0.5", "ρ = 0.9", "Mixed␣two normal"]
+for i in range(len(distributions)):
+    fig, axs = lab_src.draw_ellipse(distribution=distributions[i], title=titles[i], means=[0, 0], covariation=covs[i],
+                                    theoretical=True,
+                                    sample_sizes=samples_sizes, path_to_save=path + "RHO_" + str(rhos[i]))
+
+lab_src.draw_ellipse(distribution=mixed_distribution, title=titles[-1], means=[0, 0], covariation=mixed_covs,
+                     sample_sizes=samples_sizes, path_to_save=path + "MIXED_ELLIPSE", theoretical=False)
--- lab_5/src_lab_5.py
+++ lab_5/src_lab_5.py
@@ -1,32 +1,99 @@
 import numpy as np
 import scipy.stats as stats
-from scipy.stats import multivariate_normal
+from matplotlib.patches import Ellipse
 import matplotlib.pyplot as plt
+from numba import njit
+
+from IPython.display import set_matplotlib_formats
+
+set_matplotlib_formats('svg', 'pdf')
 
 
-def rQ(sample1: np.asarray, sample2: np.asarray):
+class MixtureModel(stats.rv_continuous):
+    def __init__(self, submodels, coefficients, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.submodels = submodels
+        self.coefficients = coefficients
+        self.dimentions = len(np.array(submodels[0].rvs(1)).shape)
+
+    def _pdf(self, x, **kwargs):
+        pdf = self.submodels[0].pdf(x) * self.coefficients[0]
+        for i in range(len(self.submodels[1:])):
+            pdf += self.submodels[i].pdf(x) * self.coefficients[i]
+        return pdf
+
+    def rvs(self, sample_size):
+        submodel_choices = np.random.choice(len(self.submodels), size=sample_size, p=self.coefficients)
+        submodel_samples = np.array([submodel.rvs(sample_size) for submodel in self.submodels])
+        sample = submodel_samples[submodel_choices, np.arange(sample_size), :]
+        return sample
+
+
+#@njit(fastmath=True)
+def rQ(sample1: np.ndarray, sample2: np.ndarray) -> float:
     med_1 = np.median(sample1)
     med_2 = np.median(sample2)
     ns = [0, 0, 0, 0]
     size = len(sample1)
     for i in range(size):
-        ns[0] = ns[0] + (sample1[i] >= med_1 and sample2 >= med_2)
-        ns[1] = ns[1] + (sample1[i] >= med_1 and sample2 <= med_2)
-        ns[2] = ns[2] + (sample1[i] <= med_1 and sample2 >= med_2)
-        ns[3] = ns[3] + (sample1[i] <= med_1 and sample2 <= med_2)
+        ns[0] = ns[0] + (sample1[i] >= med_1 and sample2[i] >= med_2)
+        ns[1] = ns[1] + (sample1[i] <= med_1 and sample2[i] >= med_2)
+        ns[2] = ns[2] + (sample1[i] <= med_1 and sample2[i] <= med_2)
+        ns[3] = ns[3] + (sample1[i] >= med_1 and sample2[i] <= med_2)
     return ((ns[0] + ns[2]) - (ns[1] + ns[3])) / size
 
 
-def get_coefficients(distribution, sample_sizes: list, N: int):
-    pearson_coefficients = np.zeros((len(sample_sizes), N))
-    quadrant_coefficients = np.zeros((len(sample_sizes), N))
-    spearman_coefficients = np.zeros((len(sample_sizes), N))
+def get_coefficients(distributions: list, sample_size: float, N: int):
+    pearson_coefficients = np.zeros((len(distributions), N))
+    quadrant_coefficients = np.zeros((len(distributions), N))
+    spearman_coefficients = np.zeros((len(distributions), N))
     for i in range(N):
-        samples = [distribution.rvs(size) for size in sample_sizes]
-        for j in range(len(sample_sizes)):
-            x, y = samples[j][:, 0], samples[j][:, 1]
+        for j in range(len(distributions)):
+            sample = distributions[j].rvs(sample_size)
+            x, y = sample[:, 0], sample[:, 1]
             pearson_coefficients[j][i] = stats.pearsonr(x, y)[0]
             quadrant_coefficients[j][i] = rQ(x, y)
             spearman_coefficients[j][i] = stats.spearmanr(x, y)[0]
     return pearson_coefficients, quadrant_coefficients, spearman_coefficients
 
+
+def help_find_mean(matrix: np.ndarray, rhos: list):
+    return [[np.mean(matrix[k]), np.mean(matrix[k] ** 2), np.std(matrix[k]) ** 2] for k in range(len(rhos))]
+
+
+def eigsorted(cov):
+    vals, vecs = np.linalg.eigh(cov)
+    order = vals.argsort()[::-1]
+    return vals[order], vecs[:,order]
+
+def draw_ellipse(path_to_save: str, distribution, title, means, covariation, sample_sizes: list, theoretical=False,
+                 fig=None, axs=None):
+    nstd = 4
+    if fig is None or axs is None:
+        fig, axs = plt.subplots(nrows=1, ncols=len(sample_sizes), figsize=(16,4))
+    vals_t, vecs_t = eigsorted(covariation)
+    theta_t = np.degrees(np.arctan2(*vecs_t[:, 0][::-1]))
+    w_t, h_t = nstd * np.sqrt(vals_t)
+    for i in range(len(axs)):
+        sample = distribution.rvs(sample_sizes[i])
+        x, y = sample[:, 0], sample[:, 1]
+        ell = None
+        if theoretical:
+            ell = Ellipse(xy=(means[0], means[1]), width=w_t, height=h_t, angle=theta_t, color='green')
+        else:
+            cov = np.cov(x, y)
+            vals, vecs = eigsorted(cov)
+            theta = float(np.degrees(np.arctan2(*vecs[:, 0][::-1])))
+            w, h = nstd * np.sqrt(vals)
+            ell = Ellipse(xy=(float(np.mean(x)), float(np.mean(y))), width=w, height=h, angle=theta, color='red')
+        axs[i].scatter(x, y)
+        axs[i].add_artist(ell)
+        ell.set_facecolor('none')
+        if title:
+            axs[i].set(title=title + ", n = " + str(sample_sizes[i]))
+        axs[i].set(xlim=(-4, 4))
+        axs[i].set(ylim=(-4, 4))
+
+    fig.subplots_adjust(wspace=0.75)
+    fig.savefig(path_to_save + ".pdf")
+    return fig, axs
